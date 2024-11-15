bac9f5d0e6cb5bab80e41db44f12a1461be222a2
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-11-15 12:19:45 +0200

    rename data directories

--- scripts/generate_conversations.sh
+++ scripts/generate_conversations.sh
@@ -4,8 +4,8 @@ THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT_DIR="$(dirname "$THIS_DIR")"
 SDF_DIR="$PROJECT_ROOT_DIR/synthetic_discussion_framework/src"
 
-INPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_input/conv_data/generated"
-OUTPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_output/collective_constitution"
+INPUT_DIR="$PROJECT_ROOT_DIR/data/discussions_input/conv_data/generated"
+OUTPUT_DIR="$PROJECT_ROOT_DIR/data/discussions_output/collective_constitution"
 
 MODEL_PATH="$PROJECT_ROOT_DIR/models/llama-3-8B-instruct.gguf"
 
@@ -16,11 +16,11 @@ mkdir -p "$LOG_DIR"
 mkdir -p "$OUTPUT_DIR"
 
 bash "$SDF_DIR/scripts/conversation_execute_all.sh" \
-    --input_dir "$PROJECT_ROOT_DIR/data/generated_discussions_input/conv_data/generated" \
+    --input_dir "$PROJECT_ROOT_DIR/data/discussions_input/conv_data/generated" \
     --output_dir  "$OUTPUT_DIR" \
     --model_path "$MODEL_PATH" \
-    --max_tokens 500 \
-    --ctx_width_tokens 4056 \
+    --max_tokens 400 \
+    --ctx_width_tokens 3000 \
     --inference_threads 10 \
     --gpu_layers 9  \
     2>&1 | tee -a "$LOG_DIR/$CURRENT_DATE.txt"
