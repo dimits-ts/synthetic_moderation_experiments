ea9c8c95ef997bae0b22153a6cc6f8e95fd35869
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-11-15 12:07:39 +0200

    merge and create new experiment

--- scripts/conversation_personalized.sh
+++ scripts/conversation_personalized.sh
@@ -5,13 +5,12 @@ PROJECT_ROOT_DIR="$(dirname "$THIS_DIR")"
 SDF_DIR="$PROJECT_ROOT_DIR/synthetic_discussion_framework/src"
 
 INPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_input/conv_data/generated"
-OUTPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_output/new"
+OUTPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_output/collective_constitution"
 
 MODEL_PATH="$PROJECT_ROOT_DIR/models/llama-3-8B-instruct.gguf"
 
 LOG_DIR="$PROJECT_ROOT_DIR/logs"
 CURRENT_DATE=$(date +'%Y-%m-%d')
-OUTPUT_DIR="$PROJECT_ROOT_DIR/data/generated_discussions_output/arguments"
 
 mkdir -p "$LOG_DIR"
 mkdir -p "$OUTPUT_DIR"
@@ -21,7 +20,7 @@ bash "$SDF_DIR/scripts/conversation_execute_all.sh" \
     --output_dir  "$OUTPUT_DIR" \
     --model_path "$MODEL_PATH" \
     --max_tokens 500 \
-    --ctx_width_tokens 2048 \
+    --ctx_width_tokens 4056 \
     --inference_threads 10 \
-    --gpu_layers 10  \
+    --gpu_layers 9  \
     2>&1 | tee -a "$LOG_DIR/$CURRENT_DATE.txt"
