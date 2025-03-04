e14f298f9628c7bceb08476bac492066623097dd
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-03-04 13:36:48 +0200

    replace variance plot

--- notebooks/tasks/stats.py
+++ notebooks/tasks/stats.py
@@ -1,5 +1,7 @@
 import itertools
+
 import numpy as np
+import pandas as pd
 from rouge_score import rouge_scorer
 
 
@@ -14,4 +16,15 @@ def pairwise_rougel_similarity(comments: list[str]) -> float:
     for c1, c2 in itertools.combinations(comments, 2):
         scores.append(scorer.score(c1.lower(), c2.lower())["rougeL"].fmeasure)
     return float(np.mean(scores)) if scores else np.nan
-    
+
+
+def discussion_var(
+    df: pd.DataFrame, discussion_key_col: str, comment_key_col: str, val_col: str
+) -> pd.Series:
+    comment_var_df = (
+        df.groupby([discussion_key_col, comment_key_col])[val_col]
+        .agg("std")
+        .reset_index()
+    )
+    discussion_var_df = comment_var_df.groupby(discussion_key_col)[val_col].agg("mean")
+    return discussion_var_df
