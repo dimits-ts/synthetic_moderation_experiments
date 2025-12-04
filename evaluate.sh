58efe104cc3076a630e05674ad4d9c460f001a22
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-12-04 11:40:57 +0200

    move main output to dedicated directory

--- evaluate.sh
+++ evaluate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 python eval/create_datasets.py \ 
     --discussions-root-dir data/discussions_output/ \
-    --output-dir data
+    --output-dir data/main_output
 
 python eval/toxicity.py \
-    --input-csv data/vmd.csv \
+    --input-csv data/main_output/vmd.csv \
     --output-path data/eval_output/vmd.csv \
     --api-key-path perspective.key
 
 python eval/toxicity.py \
-    --input-csv data/ablatyion.csv \
+    --input-csv data/main_output/ablation.csv \
     --output-path data/eval_output/ablation.csv \
     --api-key-path perspective.key
 
